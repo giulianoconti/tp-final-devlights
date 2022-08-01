62fb0591f243dfece1f2b30028506912b0b7a6f2
GiuliannT <giuliconti1@gmail.com>
2022-08-01 18:21:22 -0300

    scroll up if the page changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppRouter } from "./AppRouter";
+import { AppBrowserRouter } from "./AppBrowserRouter";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <AppBrowserRouter />
   </React.StrictMode>
 );
