9eeb0ac1758ed29bae5457057582bff2111d8bda
GiuliannT <giuliconti1@gmail.com>
2022-08-01 16:16:17 -0300

    HomeScreen changes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,23 +5,21 @@ import { ContactScreen } from "./pages/ContactScreen";
 import { LearnHowToPlayScreen } from "./pages/LearnHowToPlayScreen";
 import { GameScreen } from "./pages/GameScreen";
 import AboutUsScreen from "./pages/Nosotros";
-/* import ApiTest from "./pages/ApiTest"; */
-import HomeScreen from "./pages/HomeScreen"
+import { HomeScreen } from "./pages/HomeScreen";
 import NotFound404Screen from "./pages/NotFound404Screen";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar /> 
-        <Routes>
-          <Route path="/" element={ <HomeScreen /> } />
-          <Route path="/aprender-a-jugar" element={<LearnHowToPlayScreen />} />
-          <Route path="/juego" element={<GameScreen />} />
-          {/* <Route path="/juego" element={<ApiTest /> } />     */}      
-          <Route path="/contacto" element={<ContactScreen />} />
-          <Route path="/sobre-nosotros" element={<AboutUsScreen />} />
-          <Route path="*" element={ <NotFound404Screen /> } />          
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/aprender-a-jugar" element={<LearnHowToPlayScreen />} />
+        <Route path="/juego" element={<GameScreen />} />
+        <Route path="/contacto" element={<ContactScreen />} />
+        <Route path="/sobre-nosotros" element={<AboutUsScreen />} />
+        <Route path="*" element={<NotFound404Screen />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
